b0daa133e323e738739c9847b23bc7ab957c03ff
PushpaPriya-29 <151449400+PushpaPriya-29@users.noreply.github.com>
2025-10-24 10:32:40 +0530

    driver_commit

---
+++ drivermonitor.c
@@ -0,0 +1,66 @@
+#include "headers.h"
+drowsiness d;
+blind_spot bs;
+pthread_mutex_t lock;
+void* driver_drowsiness(void* arg){
+	
+	//srand(time(NULL));
+	while(1){
+		pthread_mutex_lock(&lock);
+		d.eye_closure=((float)rand()/RAND_MAX)*100;
+		d.yawn=((float)rand()/RAND_MAX)*101;
+		if(d.eye_closure>75 || d.yawn>60){
+			d.drowsy_flag=1;
+			printf("ALERT: Driver feeling drowsy!!\n");
+		}
+		else{
+			d.drowsy_flag=0;
+		}
+		printf("Eye closure: %.2f%% Yawn_level: %.2f%% Flag: %d\n",d.eye_closure,d.yawn,d.drowsy_flag);
+		
+		pthread_mutex_unlock(&lock);
+		sleep(1);
+	}
+	return NULL;
+}
+void* blindspot(void*){
+	
+	
+	while(1){
+		pthread_mutex_lock(&lock);
+		bs.leftblind=rand()%2;
+		bs.rightblind=rand()%2;
+		if(bs.leftblind || bs.rightblind){
+			bs.blindspot_flag=1;
+			printf("Vehicle detected in blindspot!\n");
+		}
+		else{
+			bs.blindspot_flag=0;
+		}
+		printf("Blindspot: %d %d Flag: %d\n",bs.leftblind,bs.rightblind,bs.blindspot_flag);
+		
+		pthread_mutex_unlock(&lock);
+		sleep(1);
+	}
+	return NULL;
+}
+int main(){
+	srand(time(NULL));
+	pthread_mutex_init(&lock,NULL);
+	pthread_t drowsy_thread,bspot_thread;
+	//while(1){
+		pthread_create(&drowsy_thread,NULL,driver_drowsiness,NULL);
+		pthread_create(&bspot_thread,NULL,blindspot,NULL);
+	
+		pthread_join(drowsy_thread,NULL);
+		pthread_join(bspot_thread,NULL);
+		/*if(d.drowsy_flag==1){
+			printf("ALERT: Driver feeling drowsy!!\n");
+		}
+		if(bs.blindspot_flag==1){
+			printf("Vehicle detected in blindspot!\n");
+		}*/
+		sleep(1);
+	//}	
+	return 0;
+}
